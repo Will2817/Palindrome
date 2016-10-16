e37a5f2634d3919fbab0918e12fba9503af3f12c
William <william_clifford4@hotmail.com>
2016-10-15 22:08:26 -0400

    Updated MessageService comments and Palindrome check fix
    Palindrome fix: needed to lowercase string so 'Deed' with return 'true'

--- app/message/messageService.js
+++ app/message/messageService.js
@@ -1,14 +1,16 @@
 var Message = require('./message.js');
 
-// use mongoose to get all messages in the database
+// Fetch a message by id
 exports.getMessage = function(message_id,callback){
     Message.findById(message_id,callback)
 }
 
+// Fetch all messages
 exports.getMessages = function (callback) {
     Message.find().exec(callback);
 }
 
+//Create a message
 exports.createMessage = function (message, callback) {
     Message.create({
         text: message,
@@ -16,6 +18,7 @@ exports.createMessage = function (message, callback) {
     }, callback);
 }
 
+//Update a message text by id
 exports.updateMessage = function(message_id,text,callback){
     Message.findById(message_id,function (err, message){
         if (err) {
@@ -29,14 +32,18 @@ exports.updateMessage = function(message_id,text,callback){
     });
 }
 
+//Remore a message
 exports.removeMessage = function (message_id, callback) {
     Message.remove({
         _id: message_id
     }, callback);
 }
 
+//Check if a message is a palindrome
 exports.isPalindrome = function (message) {
-    var message = message.replace(/[^a-zA-Z]/g, "");
+    //Remove all special characters and lowercase remaining letters
+    var message = message.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    //Compare characters from the outside in
     for (i = 0; i < message.length/2; i++){
         if (message.charAt(i) != message.charAt(message.length-1-i)) {
             return false;
